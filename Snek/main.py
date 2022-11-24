fe1354e6559ad673cc369c628f175676e6eaa30c
WhineyMonkey10 <71403599+WhineyMonkey10@users.noreply.github.com>
2022-11-24 11:37:27 +0100

    Class & Movement
    - Moved almost all game functions into 'game' class, not fully done that though, still need to update the names for the function calls and need to remove some of the variables inbetween.
    - Made movement function better

--- Snek/main.py
+++ Snek/main.py
@@ -24,114 +24,114 @@ score_turtle.ht()
 snek = []
 food = None
 
+class game:
+    def create_food():
+        global food
+        food = sprite.clone()
+        food.color('red')
+        food.shape("tren.gif")
 
-def create_food():
-    global food
-    food = sprite.clone()
-    food.color('red')
-    food.shape("tren.gif")
-
-    random_x = random.randint(-7, 7) * 44  # Generating random x coord for apple
-    random_y = random.randint(-7, 7) * 44  # Generating random x coord for apple
-    food.goto(random_x, random_y)
-
-
-running = True  # to keep track of weather the game is running
+        random_x = random.randint(-7, 7) * 44  # Generating random x coord for apple
+        random_y = random.randint(-7, 7) * 44  # Generating random x coord for apple
+        food.goto(random_x, random_y)
 
 
-def create_body(x, y):
-    body = sprite.clone()  # create the body
-    body.color('green')  # makes the snek green
-    body.shape("head.gif")
-    body.goto(x, y)  # moving the body
-
-    body.speed(0)
-    snek.append(body)  # adding the body to the snake (so it can get bigger)
+    running = True  # to keep track of weather the game is running
 
 
-direction = 0
+    def create_body(x, y):
+        body = sprite.clone()  # create the body
+        body.color('green')  # makes the snek green
+        body.shape("head.gif")
+        body.goto(x, y)  # moving the body
+
+        body.speed(0)
+        snek.append(body)  # adding the body to the snake (so it can get bigger)
 
 
-def move():
-    global running
-    tail = snek[len(snek) - 1]  # get the tail of the snake
-    head = snek[0]  # get the head of the snake
-    x = head.xcor()  # get the x coordinate of the snake head
-    y = head.ycor()  # get the y coordinate of the snake head
-    size = 44  # size of square is 44 px
+    direction = 0
 
-    if direction == 'down':
-        tail.goto(x, y - size)  # tail goes 22 pixels down
-        tail.shape("head.gif")
-    elif direction == 'up':
-        tail.goto(x, y + size)  # tail goes 22 pixels up
-        tail.shape("head.gif")
-    elif direction == 'right':
-        tail.goto(x + size, y)  # tail goes 22 pixels right
-        tail.shape("head.gif")
-    elif direction == 'left':
-        tail.goto(x - size, y)  # tail goes 22 pixels left
-        tail.shape("head.gif")
-    for i in range(len(snek) - 1):
-        snek[i].shape("body.gif")
 
-    for i in range(1, len(snek)):
-        if x == snek[i].xcor() and y == snek[i].ycor():
+    def move():
+        global running
+        tail = snek[len(snek) - 1]  # get the tail of the snake
+        head = snek[0]  # get the head of the snake
+        x = head.xcor()  # get the x coordinate of the snake head
+        y = head.ycor()  # get the y coordinate of the snake head
+        size = 44  # size of square is 44 px
+
+        if direction == 'down':
+            tail.goto(x, y - size)  # tail goes 22 pixels down
+            tail.shape("head.gif")
+        elif direction == 'up':
+            tail.goto(x, y + size)  # tail goes 22 pixels up
+            tail.shape("head.gif")
+        elif direction == 'right':
+            tail.goto(x + size, y)  # tail goes 22 pixels right
+            tail.shape("head.gif")
+        elif direction == 'left':
+            tail.goto(x - size, y)  # tail goes 22 pixels left
+            tail.shape("head.gif")
+        for i in range(len(snek) - 1):
+            snek[i].shape("body.gif")
+
+        for i in range(1, len(snek)):
+            if x == snek[i].xcor() and y == snek[i].ycor():
+                running = False
+
+        if x >= (screen.screensize()[0] - 2 * size) or x <= (- screen.screensize()[0] + 2 * size):
+            x = -x
+            running = False
+        if y >= (screen.screensize()[1] + size) or y <= (- screen.screensize()[1] - size):
+            y = -y
             running = False
 
-    if x >= (screen.screensize()[0] - 2 * size) or x <= (- screen.screensize()[0] + 2 * size):
-        x = -x
-        running = False
-    if y >= (screen.screensize()[1] + size) or y <= (- screen.screensize()[1] - size):
-        y = -y
-        running = False
+
+        snek.insert(0, tail)  # move tail to the head
+        snek.pop()  # Remove the last element of the list
 
 
-    snek.insert(0, tail)  # move tail to the head
-    snek.pop()  # Remove the last element of the list
+    def movement(dir):
+        if dir == "up":
+            global direction
+            if not direction == 'down':
+                direction = 'up'
+
+        elif dir == "down":
+            global direction
+            if not direction == 'up':
+                direction = 'down'
 
 
-def up():
-    global direction
-    if not direction == 'down':
-        direction = 'up'
+        elif dir == "right"
+            global direction
+            if not direction == 'left':
+                direction = 'right'
 
 
-def down():
-    global direction
-    if not direction == 'up':
-        direction = 'down'
+        elif dir == "left"
+        global direction
+        if not direction == 'right':
+            direction = 'left'
 
+    def listen():
+        screen.onkey(up, "w")
+        screen.onkey(left, "a")
+        screen.onkey(down, "s")
+        screen.onkey(right, "d")
 
-def right():
-    global direction
-    if not direction == 'left':
-        direction = 'right'
+        screen.onkey(up, "Up")
+        screen.onkey(left, "Left")
+        screen.onkey(down, "Down")
+        screen.onkey(right, "Right")
+        screen.listen()
 
+    listen()
 
-def left():
-    global direction
-    if not direction == 'right':
-        direction = 'left'
-
-def listen():
-    screen.onkey(up, "w")
-    screen.onkey(left, "a")
-    screen.onkey(down, "s")
-    screen.onkey(right, "d")
-
-    screen.onkey(up, "Up")
-    screen.onkey(left, "Left")
-    screen.onkey(down, "Down")
-    screen.onkey(right, "Right")
-    screen.listen()
-
-listen()
-
-def update():
-    global score
-    if running:
-        move()  # Move the snake at every screen refresh
+    def update():
+        global score
+        if running:
+            move()  # Move the snake at every screen refresh
         head = snek[0]
         x = head.xcor()
         y = head.ycor()
